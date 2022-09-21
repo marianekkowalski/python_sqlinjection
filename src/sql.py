5b50f9d40b07a8933b5adfe4ac7d8db07dbdd9c6
Patryk Jaworski <regalis@regalis.tech>
2022-09-21 17:03:49 +0200

    Start

---
+++ src/sql.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sqlite3 as mod
+import sys
+
+for arg in sys.argv:
+    print("ARG: %s" % arg)
+
+con = mod.connect(":memory:")
+con.execute("CREATE TABLE apius(id INTEGER PRIMARY KEY, name VARCHAR UNIQUE)")
+
+q = "INSERT INTO apius(name) VALUES('%s')"
+
+with con:
+    print("Trying to insert into database")
+    con.execute("INSERT INTO apius(name) VALUES(?)", ("Python",))
+    print("Pushing args[1]...")
+    call_list = [lambda x: x.execute(q % sys.argv[1])]
+    call_list[0](con)
+
+cur = con.execute("SELECT * FROM apius")
+print(cur.fetchall())
+
+con.close()
